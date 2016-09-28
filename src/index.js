6a723e23d8440de216e1bfbfd3c77bfeb9ffaa4e
thirdj <thirdjj@gmail.com>
2016-09-29 02:49:00 +0900

    #added material-ui #bug react onTouchTap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import App from './components/App';
 
+injectTapEventPlugin();
 const Root = () => (
   <MuiThemeProvider>
     <App />
