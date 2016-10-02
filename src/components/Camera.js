874b6a723b8c2a759980e6563cb02e80e464b75c
Choi, Jaehee <jaehee-choi@rsupport.com>
2016-10-02 17:29:44 +0900

    #added momentjs and webcam shot and connect database

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Webcam from 'react-webcam';
+import { database } from '../firebaseInit';
+import moment from 'moment';
 
 export default class Camera extends Component {
   constructor(props) {
@@ -10,14 +12,26 @@ export default class Camera extends Component {
     this.state = { screenshot: null }
   }
   shot() {
-    const screenshot = this.refs.webcam.getScreenshot();
-
+    const currentDate = moment().format("YYYYMMDD");
+    const currentTime = moment().format("HHmmss");
+    const screenshot = this.refs.webcam.getScreenshot(); // base64
+    /*
+    email
+      currentDate
+        shot time
+    */
+    database.ref().child(`thirdj/${currentDate}/${currentTime}`).set(screenshot);
+    // child('images/' + file.name).put(file, metadata);
     this.setState({ screenshot });
   }
   render() {
     return (
       <div>
-        <Webcam ref="webcam" />
+        <Webcam
+          ref='webcam'
+          width='350'
+          height='400'
+        />
         <button onClick={this.shot}>screenshot</button>
         { this.state.screenshot ? <img src={this.state.screenshot} /> : null }
       </div>
--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -4,5 +4,6 @@ import config from './firebase.config';
 export const firebaseInit = firebase.initializeApp(config);
 
 export const auth = firebaseInit.auth();
+export const database = firebaseInit.database();
 
 export const provider = new firebase.auth.GoogleAuthProvider();
