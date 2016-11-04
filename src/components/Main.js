9affe1cb24573ed47341bff63aa163814ca0bbe7
thirdj <jaehee-choi@rsupport.com>
2016-11-04 13:56:49 +0900

    #added main page separation

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,13 +5,14 @@ export default class App extends Component {
     return (
       <main className="main">
         <section>
-          <div className="row">
+          <div className="row wave">
+            여기는 음성을 말 했을때 파장이 나타날거야<br />
+            https://github.com/CaffeinaLab/SiriWaveJS
+          </div>
+          <div className="row shot">
             여기는 카메라가 나오거야
           </div>
-          <div className="row">
-            여기는 음성을 말 했을때 파장이 나타날거야
-          </div>
-          <div className="row">
+          <div className="row shot-list">
             여기는 사진 리스트가 나타날거야
           </div>
         </section>
