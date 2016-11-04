6c51a52f532d26f84658e6bd6df27f26d6b370da
thirdj <jaehee-choi@rsupport.com>
2016-11-04 11:54:42 +0900

    #improved main layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,8 +4,16 @@ export default class App extends Component {
   render() {
     return (
       <main className="main">
-        <section className="snapshot">
-          shot
+        <section>
+          <div className="row">
+            여기는 카메라가 나오거야
+          </div>
+          <div className="row">
+            여기는 음성을 말 했을때 파장이 나타날거야
+          </div>
+          <div className="row">
+            여기는 사진 리스트가 나타날거야
+          </div>
         </section>
       </main>
     );
